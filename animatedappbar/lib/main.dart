f45a8a155d299cbebd99ea126ddf8c3f5c6b7f20
Rodrigo Ferreira <rodrigoferreira@MacBook-Air-de-Rodrigo.local>
2022-07-21 21:32:11 -0300

    .

---
+++ animatedappbar/lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text(widget.title),
+      // ),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+              flexibleSpace: FlexibleSpaceBar(
+                title: const Text('sasasasasa'),
+                background: Container(
+                    height: 400, width: double.infinity, color: Colors.red),
+              ),
+              pinned: false,
+              expandedHeight: MediaQuery.of(context).size.height * 0.305),
+          SliverList(
+              delegate: SliverChildListDelegate([
+            Container(
+              color: Colors.green,
+              height: 200,
+              width: double.infinity,
+            ),
+            Container(
+              color: Colors.yellow,
+              height: 650,
+              width: double.infinity,
+            )
+          ]))
+        ],
+      ),
+    );
+  }
+}
